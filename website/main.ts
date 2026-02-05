1023e7f9bb20354ec50073aef9e8d9070396c668
kotsuban <anthonykotsuban@gmail.com>
2026-02-05 04:35:47 +0100

    Major UI + Content update
    Update border shader.
    Update text font + colors.
    Update description.
    Remove qrcode from the main page.
    Remove social icons from the links section.

--- website/main.ts
+++ website/main.ts
@@ -1,8 +1,5 @@
 import { resizeCanvas, createWebGLProgram, getRectFromElementRelative } from "@/src/util/canvas"
 
-const footerEl = document.querySelector(".footer-left") as HTMLDivElement;
-footerEl.textContent = (footerEl.textContent as string).replace("2025-11-26T02:40:46.422Z", new Date().toISOString())
-
 const contentEl = document.querySelector(".content") as HTMLDivElement;
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 const gl = canvas.getContext("webgl2")!;
@@ -14,43 +11,38 @@ function createRenderer(gl: WebGL2RenderingContext, canvas: HTMLCanvasElement) {
   void main() {
     gl_Position = vec4(aPos, 0.0, 1.0);
   }`;
+  // Shout out to XOR for noise and turbulence examples: https://www.shadertoy.com/view/XtXXD8.
   const fs = `#version 300 es
     precision highp float;
-    out vec4 fragColor;
+    out vec4 o;
 
     uniform vec2 uResolution;
     uniform vec2 uPos;
     uniform float uTime;
+    uniform vec2 uMouse;
 
-    float ccross(vec2 offsetPos, vec4 sideLimits) {
-        vec2 p = (gl_FragCoord.xy - offsetPos) / uResolution.y * 2.0;
-
-        for(float i = 0.0; i < 11.0; i++) {
-            p.x += sin(p.y + i + uTime * 0.09);
-            p *= mat2(6.0, -8.0, 5.0, 5.0) / 7.5;
-        }
-
-        float pattern = sin(p.x * 0.9 + p.y * 0.1 + uTime * 0.01) * 0.1 + 0.16;
-        vec2 rel = (gl_FragCoord.xy - offsetPos) / uResolution.y;
-        float blend = 0.2 / (abs(rel.y) * abs(rel.x) * 90.0 + 0.001);
-        blend *= pattern;
-        
-        blend *= smoothstep(sideLimits.x, 0.0, rel.x);
-        blend *= 1.0 - smoothstep(0.0, sideLimits.y, rel.x);
-        blend *= smoothstep(sideLimits.z, 0.0, rel.y);
-        blend *= 1.0 - smoothstep(0.0, sideLimits.w, rel.y);
-        
-        return blend;
+    float hash(vec2 n) {return fract(cos(dot(n,vec2(36.26,73.12)))*354.63);}
+    float noise(vec2 n) {
+      vec2 fn = floor(n), sn = smoothstep(vec2(0.0),vec2(1.0),fract(n));
+      return mix(mix(hash(fn),hash(fn+vec2(1.0,0.0)),sn.x),mix(hash(fn+vec2(0.0,1.0)),hash(fn+vec2(1.0)),sn.x),sn.y);
+    }
+    float vnoise(vec2 n) {
+      return noise(n/32.0)*0.5875+noise(n/16.0)*0.2+noise(n/8.0)*0.1+noise(n/4.0)*0.05+noise(n/2.0)*0.025+noise(n)*0.0125;
     }
 
     void main() {
-        vec2 oppositePos = uResolution - uPos;
-        
-        float blend1 = ccross(uPos, vec4(-0.4, 0.7, -0.7, 0.15));
-        float blend2 = ccross(oppositePos, vec4(-0.7, 0.4, -0.15, 0.7));
-        
-        fragColor = vec4(vec3(max(blend1, blend2)), 1.0);
-    }`;
+      vec2 p=(gl_FragCoord.xy-uPos)/uResolution.y*2.;
+      for (float i=.0;i<11.;i++){p.x+=sin(p.y+i+uTime*.02);p*=mat2(9.,-8.,.6,8.)/7.3;}
+      float pat=smoothstep(.2,.6,vnoise(p*23.5+uTime*.005)*.25+.09);
+      vec2 a=(gl_FragCoord.xy-uPos)/uResolution.y;
+      vec2 b=(gl_FragCoord.xy-(uResolution-uPos))/uResolution.y;
+      float m=abs(a.x)*abs(b.x)*abs(a.y)*abs(b.y)*90.+.001;
+      float ma=.09/m*pat*smoothstep(-0.4,.0,a.x)*(1.-smoothstep(.0,1.1,a.x))*smoothstep(-0.7,0.0,a.y)*(1.-smoothstep(0.0,0.19,a.y));
+      float mb=.09/m*pat*smoothstep(-1.1,.0,b.x)*(1.-smoothstep(.0,0.4,b.x))*smoothstep(-.19,.0,b.y)*(1.0-smoothstep(.0,.7,b.y));
+
+      o = vec4(vec3(1., .85, .6) * ma + vec3(1., .85, .6) * mb, max(ma, mb));
+    }
+  `;
 
   const vertices = new Float32Array([
     -1.0, -1.0,
@@ -74,6 +66,7 @@ function createRenderer(gl: WebGL2RenderingContext, canvas: HTMLCanvasElement) {
   const uTime = gl.getUniformLocation(program, "uTime");
   const uPos = gl.getUniformLocation(program, "uPos");
   const uRes = gl.getUniformLocation(program, 'uResolution');
+  const uMouse = gl.getUniformLocation(program, "uMouse");
 
   let startTime = performance.now();
 
@@ -92,10 +85,20 @@ function createRenderer(gl: WebGL2RenderingContext, canvas: HTMLCanvasElement) {
     const { x, y } = getRectFromElementRelative(contentEl, canvas);
     gl.useProgram(program);
     gl.uniform2f(uRes, canvas.width, canvas.height);
-    gl.uniform2f(uPos, x - 50, y + 40);
+    gl.uniform2f(uPos, x - 30, y + 10);
+  }
+
+  function mousemove(e: MouseEvent) {
+    const rect = canvas.getBoundingClientRect();
+    const x = (e.clientX - rect.left) / rect.width * 2 - 1;
+    const y = 1 - (e.clientY - rect.top) / rect.height * 2;
+
+    gl.useProgram(program);
+    gl.uniform2f(uMouse, x, y);
   }
 
   window.addEventListener("resize", resize);
+  window.addEventListener("mousemove", mousemove);
   resize();
 
   requestAnimationFrame(frame);
